e1b99f300ae36001acb025537c351a1365e90b5c
“Sam <sam.alsmadi@gmail.com>
2018-06-01 22:17:20 -0400

    Add redux logger/redux dev tools extension

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
-import { createStore, combineReducers, applyMiddleware} from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { createLogger } from 'redux-logger';
 import 'tachyons';
 
 import './index.css';
@@ -15,7 +16,10 @@ const rootReducer = combineReducers({
   robotGenerator: robotGeneratorReducer
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const logger = createLogger();
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 const app = (
   <Provider store={store} >
